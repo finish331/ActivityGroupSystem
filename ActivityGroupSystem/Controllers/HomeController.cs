657bfe0feedffa9fa991213220562f568f918331
willie_chen <willie_chen@gss.com.tw>
2020-05-01 15:31:20 +0800

    '連接firebase測試'

--- ActivityGroupSystem/Controllers/HomeController.cs
+++ ActivityGroupSystem/Controllers/HomeController.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Firebase.Database;
+using Firebase.Database.Query;
+using System.Linq;
+using System.Threading.Tasks;
+using ActivityGroupSystem.Models;
 
 namespace ActivityGroupSystem.Controllers
 {
@@ -13,10 +18,39 @@ namespace ActivityGroupSystem.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public async Task<ActionResult> About()
         {
             ViewBag.Message = "Your application description page.";
 
+            //Simulate test user data and login timestamp
+            var userId = "12345";
+            var currentLoginTime = DateTime.UtcNow.ToString("MM/dd/yyyy HH:mm:ss");
+
+            //Save non identifying data to Firebase
+            var currentUserLogin = new LoginData() { TimestampUtc = currentLoginTime };
+            var firebaseClient = new FirebaseClient("https://activitygroup-74f7f.firebaseio.com/");
+            var result = await firebaseClient
+              .Child("Users/" + userId + "/Logins")
+              .PostAsync(currentUserLogin);
+
+            //Retrieve data from Firebase
+            var dbLogins = await firebaseClient
+              .Child("Users")
+              .Child(userId)
+              .Child("Logins")
+              .OnceAsync<LoginData>();
+
+            var timestampList = new List<DateTime>();
+
+            //Convert JSON data to original datatype
+            foreach (var login in dbLogins)
+            {
+                timestampList.Add(Convert.ToDateTime(login.Object.TimestampUtc).ToLocalTime());
+            }
+
+            //Pass data to the view
+            ViewBag.CurrentUser = userId;
+            ViewBag.Test = timestampList.OrderByDescending(x => x);
             return View();
         }
 
