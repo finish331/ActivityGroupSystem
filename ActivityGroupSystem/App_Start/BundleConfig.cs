18bc73b7b601f910baac95a7159c9ed8c71f8874
willie_chen <willie_chen@gss.com.tw>
2020-05-07 23:25:46 +0800

    kendoUI 整合完成

--- ActivityGroupSystem/App_Start/BundleConfig.cs
+++ ActivityGroupSystem/App_Start/BundleConfig.cs
@@ -22,9 +22,13 @@ namespace ActivityGroupSystem
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/kendoUI").Include(
+                      "~/Scripts/kendo.all.min.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
         }
     }
 }
---
+++ ActivityGroupSystem/Scripts/index.js
@@ -0,0 +1,19 @@
+﻿
+$(document).ready(function () {
+    debugger;
+    $("#logout_button").kendoButton();
+    $("#detail_form").kendoValidator().data("kendoValidator");
+
+    $("#detail_form").kendoWindow({
+        width: "600px",
+        title: "Book Detail Window",
+        visible: false,
+        actions: ["Pin", "Minimize", "Maximize", "Close"],
+        modal: true
+    }).data("kendoWindow");
+});
+
+$("#logout_button").click(function () {
+    $("#detail_form").data('kendoValidator').hideMessages();
+    $("#detail_form").data('kendoWindow').center().open();
+});
