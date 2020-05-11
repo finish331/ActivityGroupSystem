b14e41f9a45277a50d63299260e3b0307b993f2c
seth8729 <tom8729hsu@yahoo.com.tw>
2020-05-11 14:52:55 +0800

    前端都接好 查看他人資料不知接哪暫放在_layout裡

--- ActivityGroupSystem/Scripts/index.js
+++ ActivityGroupSystem/Scripts/index.js
@@ -1,6 +1,5 @@
 ﻿$(document).ready(function () {
     debugger;
-    $("#logout_button").kendoButton();
     $("#btn_create_activity").kendoButton();
     $("#btn_manage_activity").kendoButton();
     $("#btn_view_join_activity").kendoButton();
@@ -65,7 +64,25 @@
             }
         ]
     });
+
+    
+   
+    
 });
+
+/*$("#logout_button").click(function () {
+    $("#register_form").kendoValidator().data("kendoValidator");
+    $("#register_form").kendoWindow({
+        width: "600px",
+        title: "Register",
+        visible: false,
+        actions: ["Minimize", "Maximize", "Close"],
+        content: "Hall/Register",
+        modal: true
+    });
+    $("#register_form").data('kendoValidator').hideMessages();
+    $("#register_form").data('kendoWindow').center().open();
+});*/
 
 //活動Grid中參加活動的按鈕
 $("#activity_grid").on("click", ".btn-join-button", function (e) {
---
+++ ActivityGroupSystem/Scripts/layout.js
@@ -0,0 +1,58 @@
+﻿
+$(document).ready(function () {
+    debugger;
+    $("#register_button").kendoButton();
+    $("#memberInfo_button").kendoButton();
+    $("#test").kendoButton();
+});
+
+$("#register_button").click(function () {
+    $("#register_form").kendoValidator().data("kendoValidator");
+    $("#register_form").kendoWindow({
+        width: "600px",
+        top: "100px",
+        title: "Register",
+        visible: false,
+        actions: ["Minimize", "Maximize", "Close"],
+        content: "Hall/Register",
+        modal: true
+    });
+    //打開window
+    $("#register_form").data('kendoValidator').hideMessages();
+    $("#register_form").data('kendoWindow').center().open();
+});
+
+$("#memberInfo_button").click(function () {
+    $("#memberInfo_form").kendoValidator().data("kendoValidator");
+    $("#memberInfo_form").kendoWindow({
+        width: "900px",
+        title: "Member Information",
+        visible: false, //設定此介面一開始是否可看見
+        actions: ["Minimize", "Maximize", "Close"],
+        content: "Hall/MemberInfo",
+        position: {
+            top: "20%",
+            left: "30%"
+        },     
+        modal: true, //操作kendoWindow時，其他元件無法操作
+    }).data("kendoWindow");
+    
+    //打開window
+    $("#memberInfo_form").data('kendoValidator').hideMessages();
+    $("#memberInfo_form").data('kendoWindow').open();
+});
+
+$("#test").click(function () {
+    $("#checkInfo_form").kendoValidator().data("kendoValidator");
+    $("#checkInfo_form").kendoWindow({
+        width: "500px",
+        title: "Member Information",
+        visible: false, //設定此介面一開始是否可看見
+        actions: ["Minimize", "Maximize", "Close"],
+        content: "Hall/OtherMemberInfo",
+        modal: true, //操作kendoWindow時，其他元件無法操作
+    }).data("kendoWindow");
+    //打開window
+    $("#checkInfo_form").data('kendoValidator').hideMessages();
+    $("#checkInfo_form").data('kendoWindow').center().open();
+});
