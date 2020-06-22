32757204d05d6af401d86c939dc828e12a08a17c
seth8729 <tom8729hsu@yahoo.com.tw>
2020-06-22 20:24:24 +0800

    登入頁面開啟註冊頁面

--- ActivityGroupSystem/Scripts/login.js
+++ ActivityGroupSystem/Scripts/login.js
@@ -1,5 +1,24 @@
 ﻿$(document).ready(function () {
     $("#register_button").kendoButton();
- 
-   
 });
+
+$("#register_button").click(function () {
+    $("#register_form").kendoValidator().data("kendoValidator");
+    $("#register_form").kendoWindow({
+        width: "600px",
+        top: "100px",
+        title: "Register",
+        visible: false,
+        actions: ["Minimize", "Maximize", "Close"],
+        content: "Register",
+        position: {
+            top: "5%",
+            left: "35%"
+        },
+        modal: true
+    });
+    //打開window
+    $("#register_form").data('kendoValidator').hideMessages();
+    $("#register_form").data('kendoWindow').open();
+});
+
